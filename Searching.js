97379eb694948f2828be7257ccfe8aa752660c48
Rasujon3 <rasujon3@gmail.com>
2022-11-19 22:35:37 +0600

    2. Binary Search

--- Searching.js
+++ Searching.js
@@ -14,3 +14,34 @@ function linearSearch(arr, target) {
 }
 
 console.log(linearSearch(arr, target));
+console.log(linearSearch(["red", "green", "blue"], "Blue"));
+
+// 2. Binary Search -> (Condition 1.Sorted)
+let arr2 = [10, 20, 30, 40, 50, 60, 70, 80];
+
+function binarySearch(arr, target) {
+  // mid
+  let start = 0;
+  let end = arr.length - 1;
+  let mid;
+
+  while (start <= end) {
+    mid = Math.floor((start + end) / 2);
+    // 1. arr[mid]===target
+    // 2. small
+    // 3. large
+
+    if (arr[mid] === target) {
+      return "Data found at index " + mid;
+    } else if (arr[mid] < target) {
+      start = mid + 1;
+      mid = Math.floor((start + end) / 2);
+    } else {
+      end = mid - 1;
+      mid = Math.floor((start + end) / 2);
+    }
+  }
+  return "Data not found";
+}
+
+console.log(binarySearch(arr2, 83));
