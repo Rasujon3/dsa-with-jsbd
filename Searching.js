873abc9d9d68f54282b3e414a290bf0269956334
Rasujon3 <rasujon3@gmail.com>
2022-11-23 19:58:42 +0600

    asc desc in binary search

--- Searching.js
+++ Searching.js
@@ -45,3 +45,66 @@ function binarySearch(arr, target) {
 }
 
 console.log(binarySearch(arr2, 83));
+console.log(binarySearch([25, 20, 15, 12, 10, 5, 2, -2, -4], 10));
+console.log(binarySearch(["apple", "banana", "orange"], "banana"));
+
+// assignment:
+// Blog:
+// 1. Linear Search
+// 2. Binary Search
+// 3. Comparison
+
+// 2. Binary Search with asc dsc -> (Condition 1.Sorted)
+let arr3 = [10, 20, 30, 40, 50, 60, 70, 80];
+
+function binarySearch2(arr, target) {
+  // mid
+  let start = 0;
+  let end = arr.length - 1;
+  let mid;
+  let asc;
+  if (arr[start] < arr[end]) {
+    asc = true;
+  } else {
+    asc = false;
+  }
+
+  while (start <= end) {
+    mid = Math.floor((start + end) / 2);
+    // 1. arr[mid]===target
+    // 2. small
+    // 3. large
+
+    if (arr[mid] === target) {
+      return "Data found at index " + mid;
+    }
+    if (asc) {
+      if (arr[mid] < target) {
+        start = mid + 1;
+        // mid = Math.floor((start + end) / 2);
+      } else {
+        end = mid - 1;
+        // mid = Math.floor((start + end) / 2);
+      }
+    } else {
+      if (arr[mid] > target) {
+        start = mid + 1;
+        // mid = Math.floor((start + end) / 2);
+      } else {
+        end = mid - 1;
+        // mid = Math.floor((start + end) / 2);
+      }
+    }
+  }
+  return "Data not found";
+}
+
+console.log(binarySearch2(arr2, 83));
+console.log(binarySearch2([25, 20, 15, 12, 10, 5, 2, -2, -4], 10));
+console.log(binarySearch2(["apple", "banana", "orange"], "banana"));
+
+// assignment:
+// Blog:
+// 1. Linear Search
+// 2. Binary Search
+// 3. Comparison
