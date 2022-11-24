945bc24cbc3970faa468a33abc5964c0bfd3a854
Rasujon3 <rasujon3@gmail.com>
2022-11-25 03:25:33 +0600

    1539 problem solve

---
+++ 1539_solve.js
@@ -0,0 +1,34 @@
+// let arr = [2, 3, 4, 7, 11];
+let arr = [1, 2, 3, 4];
+let target = 2;
+
+function findKthNumber(arr, target) {
+  let missingArray = [];
+
+  let n = 1;
+  let i = 0;
+
+  while (true) {
+    if (n == arr[i]) {
+      console.log(arr[i]);
+      i++;
+      console.log(i);
+      n++;
+      console.log(n);
+    } else {
+      missingArray.push(n);
+      console.log(missingArray);
+      n++;
+      console.log(n);
+
+      if (missingArray.length > target) {
+        break;
+      }
+    }
+  }
+  return missingArray[target - 1];
+}
+
+// missingArray;
+// console.log(missingArray[target - 1]);
+console.log(findKthNumber(arr, target));
